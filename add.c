01b9f0a27b5d1a450ba15b6d45b090ad9fcd3839
Siddhanta Banerjee <39611700+Sidoryx@users.noreply.github.com>
2021-07-11 23:20:31 +0530

    Add files via upload
    Moving things forward, in this C program we add two numbers.

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter the value of first number: ");
+    scanf("%d", &a);
+    printf("Enter the value of second number: ");
+    scanf("%d", &b);
+    sum=a+b;
+    printf("The sum of %d and %d is %d", a, b, sum);
+}
